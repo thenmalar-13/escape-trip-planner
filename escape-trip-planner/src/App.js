51bc2050d803375983d06d72a501a601d65acb8e
Thenmalar <thenmalar.3472@gmail.com>
2026-09-06 12:30:37 +0530

    Added Itinerary component

---
+++ escape-trip-planner/src/App.js
@@ -0,0 +1,14 @@
+import SearchForm from "./SearchForm";
+import Itinerary from "./Itinerary";
+
+function App() {
+  return (
+    <div>
+      <h1>ESCAPE Weekend Trip Planner</h1>
+      <SearchForm />
+      <Itinerary />
+    </div>
+  );
+}
+
+export default App;
---
+++ escape-trip-planner/src/Itinerary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Itinerary() {
+  return (
+    <div>
+      <h2>Your Weekend Itinerary</h2>
+      <ul>
+        <li>Day 1: Visit local attractions</li>
+        <li>Day 2: Try popular food spots</li>
+      </ul>
+    </div>
+  );
+}
